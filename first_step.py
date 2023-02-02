9b3c333fee1cbd569ed916e44b077d6423d96434
SHz Divo <shz.divo@gmail.com>
2023-02-02 18:19:35 +0300

    chages y

--- first_step.py
+++ first_step.py
@@ -5,3 +5,13 @@ if low%2==0 and high%2==0:
     print (high//2 - low//2)
 else:
     print ((high-low)//2+1)
+##1491. Average Salary Excluding the Minimum and Maximum Salary
+
+print('\n'"1491. Average Salary Excluding the Minimum and Maximum Salary"'\n')
+
+salary = [4000,3000,1000,2000]
+salary.remove(max(salary))
+salary.remove(min(salary))
+print (sum(salary)/len(salary))
+
+##
